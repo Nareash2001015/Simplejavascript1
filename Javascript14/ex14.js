a6020f0b18faa1fbae601561b7fc447e2d40e31e
Mr.Nareash <102378093+Nareash20010150@users.noreply.github.com>
2022-04-05 07:22:42 +0530

    Add files via upload

---
+++ Javascript14/ex14.js
@@ -0,0 +1,29 @@
+var heading = document.getElementsByTagName("h2")[0];
+var noteName = document.getElementsByClassName("typeField")[0];
+var image = document.getElementById("record");
+console.log(document.getElementsByTagName("div")[0].clientWidth);
+
+function changeImage(e)
+{
+    var imgSrc = e.target.alt;
+    if(imgSrc == "record")
+    {
+        image.setAttribute("src", "D:/Javascript/javascript-and-jquery-book-code/javascript-and-jquery-book-code-0915/c06/images/pause.png");
+        image.setAttribute("alt", "pause");
+    }
+    else
+    {
+        image.setAttribute("src", "D:/Javascript/javascript-and-jquery-book-code/javascript-and-jquery-book-code-0915/c06/images/record.png");
+        image.setAttribute("alt", "record");
+    }
+}
+
+function updateHeading(e)
+{
+    heading.textContent = noteName.value;
+}
+
+image.addEventListener("click", changeImage, false);
+noteName.addEventListener("keyup", updateHeading, false);
+noteName.addEventListener("keydown", updateHeading, false);
+
